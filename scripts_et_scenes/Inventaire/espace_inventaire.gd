6be75d5b97f1e9ee4eba33d66273fe5019341605
bilzeiboube <mael.autre@gmail.com>
2026-06-12 16:29:59 +0200

    dans inventaire, fonction qui permettent de situer où est l'objet

--- scripts_et_scenes/Inventaire/espace_inventaire.gd
+++ scripts_et_scenes/Inventaire/espace_inventaire.gd
@@ -3,6 +3,7 @@ extends Control
 
 @onready var element_test = preload("res://scripts_et_scenes/Inventaire/element_test.tscn")
 
+@export var tab_inventary_zone : Array[ColorRect]
 
 @onready var tab_elements = []
 
@@ -12,9 +13,43 @@ func _input(event: InputEvent) -> void:
 		add_object(element_test)
 
 
-func add_object(object):
+func add_object(object) -> void:
 	#print("en train ajouter element")
 	var tmp = object.instantiate()
 	self.add_child(tmp)
 	tab_elements.append(object)
 	#print("fini ajouter " + tmp.name)
+
+
+func is_in_inventary(object) -> bool:
+	var tmp_pos = object.global_position
+	for i in tab_inventary_zone:
+		if ( tmp_pos.x >= i.global_position.x and tmp_pos.x <= (i.global_position.x + i.size.x) ) and ( tmp_pos.y >= i.global_position.y and tmp_pos.y <= (i.global_position.y + i.size.y) ):
+			return true
+	return false
+
+
+func is_out_left_inventary(object) -> bool:
+	if object.global_position.x < 0:
+		return true
+	return false
+
+
+func is_out_right_inventary(object) -> bool:
+	if object.global_position.x > (tab_inventary_zone[1].global_position.x + tab_inventary_zone[1].size.x):
+		return true
+	return false
+
+
+func is_out_up_inventary(object) -> bool:
+	if object.global_position.y < 0:
+		return true
+	return false
+
+
+func is_out_down_inventary(object) -> bool:
+	var tmp_pos = object.global_position
+	for i in tab_inventary_zone:
+		if ( tmp_pos.x >= i.global_position.x and tmp_pos.x <= (i.global_position.x + i.size.x) ) and ( tmp_pos.y > (i.global_position.y + i.size.y) ):
+			return true
+	return false
