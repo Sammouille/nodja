52a9b470ed5dda7bbbb7a6f0fbfa3052b3a5d64b
RaphaelNestor <98326616+RaphaelNestor@users.noreply.github.com>
2026-07-03 12:09:08 +0200

    sons mini jeu photo

--- scripts_et_scenes/mini-jeux/mj_photo.gd
+++ scripts_et_scenes/mini-jeux/mj_photo.gd
@@ -4,9 +4,14 @@ var nb_tentatives = 0
 var pos_der_photo = Vector2(576,500)
 @export var photo_retouchee : PackedScene
 @export var sprite_tampon_valide : Texture
-@export var stream : Node
+@export var stream_tampon : Node
+@export var stream_gribouille : Node
+
+@export var son_refuse : AudioStream
+@export var son_valide : AudioStream
 
 func _ready() -> void:
+	stream_tampon.stream = son_refuse
 	for i in get_children():
 		if i.is_in_group("photo"):
 			i.photo_clicked.connect(_on_photo_clicked)
@@ -14,14 +19,20 @@ func _ready() -> void:
 func _on_photo_clicked():
 	nb_tentatives += 1
 	print("tentatives :" + str(nb_tentatives))
+	stream_tampon.play()
 	if nb_tentatives >= 5:
 		print("photo retouchée")
+		stream_gribouille.play()
+		
+func _instantiate_photo_retouchee():
 		var nv_instance = photo_retouchee.instantiate()
 		nv_instance.photo_retouchee_clicked.connect(_on_photo_retouchee_clicked)
 		add_child(nv_instance)
 		nv_instance.position = pos_der_photo
 
 func _on_photo_retouchee_clicked():
+	stream_tampon.stream = son_valide
+	stream_tampon.play()
 	print("BRAVO")
 	var sprite_tampon = Sprite2D.new()
 	sprite_tampon.texture = sprite_tampon_valide
