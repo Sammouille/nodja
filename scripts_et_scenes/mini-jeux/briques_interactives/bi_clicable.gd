3a626a6099e0dbc222c1dde13e9a9c8898abefb1
RaphaelNestor <nestor.martin.raphael@gmail.com>
2026-07-24 12:26:28 +0200

    commentaires

--- scripts_et_scenes/mini-jeux/briques_interactives/bi_clicable.gd
+++ scripts_et_scenes/mini-jeux/briques_interactives/bi_clicable.gd
@@ -1,33 +1,30 @@
 extends Area2D
 class_name clicable
 
-signal photo_clicked
+signal photo_clicked # signal envoyé à la scène MJPhoto quand la photo est cliquée
 
 @export var tampon_refus : PackedScene
 
-var hovered = false
-var used = false
+var hovered = false # stocke l'info la souris est elle sur cette photo ?
+var used = false # stocke l'info cette photo a t elle déjà été cliquée ?
 
-func _ready() -> void:
+func _ready() -> void: # randomise légèrement la position et l'angle des photos
 	rotation_degrees += randi_range(-20,20)
 	position.x += randi_range(-20,20)
 	position.y += randi_range(-20,20)
 
-func _on_mouse_entered() -> void:
+func _on_mouse_entered() -> void: # passe le booléen à vrai quand la souris rentre sur la photo
 	hovered = true
-	#print("on")
 
-func _on_mouse_exited() -> void:
+func _on_mouse_exited() -> void: # passe le booléen à faux quand la souris ressort de la photo
 	hovered = false
-	#print("off")
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
-		if Input.is_action_just_pressed("clic") and hovered:
-			#print("clicgoch")
-			if !used:
+		if Input.is_action_just_pressed("clic") and hovered: # réagit si la lic est appuyé et le curseur est sur la photo
+			if !used: # chaque photo ne peut etre essayée qu'une fois dont elle réagit que si elle n'a pas encore été cliquée
 				print("photo refusée")
-				var nv_instance = tampon_refus.instantiate()
-				add_child(nv_instance)
-				photo_clicked.emit()
+				var nv_instance = tampon_refus.instantiate() # instancie le tampon de refus
+				add_child(nv_instance) # ajoute l'instance du tampo à la scène
+				photo_clicked.emit() # prévient MJPhoto qu'une nouvelle photo à été cliquée
 				used = true
--- scripts_et_scenes/mini-jeux/lumiere/lumiere.gd
+++ scripts_et_scenes/mini-jeux/lumiere/lumiere.gd
@@ -9,28 +9,22 @@ var hovered = false
 var used = false
 
 func _ready() -> void:
-	$Sprite2D.texture = lampe_eteinte
+	$Sprite2D.texture = lampe_eteinte # prend la texture de lampe éteinte
+	# randomise l'angle et la position de la lumière
 	rotation_degrees += randi_range(-50,50)
 	position.x += randi_range(-50,50)
 	position.y += randi_range(-50,50)
 
-func _on_mouse_entered() -> void:
+func _on_mouse_entered() -> void: # passe la variable à vrai quand la souris entre sur la lumière
 	hovered = true
-	#print("on")
 
-func _on_mouse_exited() -> void:
+func _on_mouse_exited() -> void: # passe la variable à faux quand la souris sort de la lumière
 	hovered = false
-	#print("off")
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
-		if Input.is_action_just_pressed("clic") and hovered:
-			if !used:
-				print("lumière allumée")
-				$Sprite2D.texture = lampe_allumee
-				light_clicked.emit()
+		if Input.is_action_just_pressed("clic") and hovered: # réagit si le clic est appuyé et la osuris est sur la lumière
+			if !used: # ne réagit que si la lumière n'a pas encore été allumée
+				$Sprite2D.texture = lampe_allumee # prend la texture de lampe allumée
+				light_clicked.emit() # prévient MJLumière qu'une nouvelle lumière à été allumée
 				used = true
-
-#func _fond_allume():
-	#print("disparu")
-	#visible = false
